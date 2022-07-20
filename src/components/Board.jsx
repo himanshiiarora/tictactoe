1e00a8a8751ee5b238a6d634b37dd9841415035f
Himanshi Arora <arorahimanshi0722@gmail.com>
2022-07-21 00:02:58 +0530

    added board state and react events

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,26 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Square from './Square'
 const Board = () => {
+
+  
+  const [board, setBoard] = useState( Array(9).fill(null));
+  const[isXNext, setIsXNext] = useState(false);
+  // isXNext is a boolean value which represents our currept player, which simply means that is X the next player or not
+
+
+
+  const handleSquareClick = (position) => {
+
+
+    // there's a problem, that suppose if we already marked a box as 'O', and if we click on it again,
+    // it will turn to 'X'. therefore to omit this behaviour, we will do the following.
+
+    if(board[position]){
+      return;
+    }
+
+
+    setBoard( (prev) => {
+      return prev.map((square, pos) => {
+        if(pos == position){
+          return isXNext ? 'X' : 'O'; //but we need to update it, else all qill be O
+        }
+
+        return square;
+      });
+    });
+
+    setIsXNext((prev) => !prev); // updation, O after X, and X after O
+  };
+  const renderSquare = position => {
+    return(
+      <Square 
+        value={board[position]}
+        onClick = {() => handleSquareClick(position)}
+      />
+    );
+  };
+
   return (
     <div className='board'>
 
         <div className='board-row'>
-            <Square value={0} />
-            <Square value={1} />
-            <Square value={2} />
+          { renderSquare(0) }
+          { renderSquare(1) }
+          { renderSquare(2) }
         </div>
 
         <div className='board-row'>
-            <Square value={3} />
-            <Square value={4} />
-            <Square value={5} />
+          { renderSquare(3) }
+          { renderSquare(4) }
+          { renderSquare(5) }
         </div>
 
 
         <div className='board-row'>
-            <Square value={6} />
-            <Square value={7} />
-            <Square value={8} />
+          { renderSquare(6) }
+          { renderSquare(7) }
+          { renderSquare(8) }
         </div>
 
     </div>
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,10 +1,20 @@
 //rafce
 import React from 'react'
 
-const Square = ({ value ,children}) => {
+const Square = ({ value ,onClick}) => {
   return (
-    <button className='square'> { value } </button>
-  )
-}
+    // <button className='square'
+      // onClick={() => {
+      //   console.log(value);}}
+      // > { value } </button>
+      
+      //or
+
+      
+    <button className='square' onClick={onClick}> 
+      { value }
+    </button>
+  );
+};
 
 export default Square
